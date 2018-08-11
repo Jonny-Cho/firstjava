36f4cc8678dd204e00b505da91618159d375f0a5
Jonny <andante2183@gmail.com>
2018-08-11 23:09:11 +0900

    Tutorial 5

---
+++ Tutorial5/src/Main.java
@@ -0,0 +1,19 @@
+
+public class Main {
+	
+	public static void main(String[] arg) {
+		String a = "I Love You.";
+		if(a.contains("Love"))
+		{
+			//포함 할 때 실행되는 부분
+			System.out.println("Me Too.");
+		}
+		else
+		{
+			//포함하지 않을 때 실행되는 부분
+			System.out.println("I Hate You.");
+			
+		}
+	}
+
+}
